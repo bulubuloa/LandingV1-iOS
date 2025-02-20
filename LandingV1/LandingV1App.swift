17fa11790e1947dfd71bedb091a54323708a5fb3
HoangQuach <quachhoangqb@gmail.com>
2025-02-20 16:36:40 +0700

    Initialize

---
+++ LandingV1/LandingV1App.swift
@@ -0,0 +1,17 @@
+//
+//  LandingV1App.swift
+//  LandingV1
+//
+//  Created by HoangQ on 20/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LandingV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
